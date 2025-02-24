f0f75ba2940b940533649ff232510cd0056da728
Zogam <derekchizogam@gmail.com>
2025-02-24 09:48:02 +0100

    Assignment for JS Class

---
+++ Assignment.js
@@ -0,0 +1,17 @@
+// Declare variables and assign numbers
+let num1 = 50;
+let num2 = 5;
+
+//To Perform operations, we 
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+// To Display the results we call on any of the following 
+console.log("Addition = ", addition);
+console.log("Subtraction = ", subtraction);
+console.log("Multiplication = ", multiplication);
+console.log("Division = ", division);
+console.log("Modulus = ", modulus);
