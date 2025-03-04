95fb08725834c400815e84dbea41c845548c61a8
Zogam <derekchizogam@gmail.com>
2025-03-04 02:06:09 +0100

    Added New Files

---
+++ Functions.js
@@ -0,0 +1,19 @@
+// This function returns multiplication of three numbers
+function multiplesofNumbers(num1,num2, num3){
+    const multiple = (num1 * num2 * num3)
+    console.log(multiple)
+}
+multiplesofNumbers(2,5,7)
+
+// This is another way to write functions in JS 
+multiplesOfNumbers = (num1, num2, num3) => {
+    const multiple = (num1 * num2 * num3)
+    console.log(multiple)
+}
+multiplesOfNumbers(2,6,7)
+
+// This is also another way to write functions 
+(function calculateProduct(num1, num2, num3) {
+    const product = num1 * num2 * num3;
+    console.log(product);
+})(2, 8, 0)
---
+++ Operators.js
@@ -0,0 +1,23 @@
+// There are different types of operators. The Arithmetic ones are the ones we use for calculations like +, -, *, %, /
+// The comparison operators are used to determine the equality or difference between values or variables like ==, ===, !=
+// Logical operators are used to determine the logic between values or variables like &&, ||
+// Assignment operators are used to assign values to variables
+
+let x = 5 + 5
+let y = 10 - 6
+console.log ('The new result is', x)
+console.log ('The new result is', y) 
+
+let f = 4 
+console.log (f == 8) // This should return false
+
+let m = 87
+console.log ( m === '87') // This should return false
+
+let person1 = 'James'
+let person2 = 'Victor'
+if (person1 === 'James' && person2 === 'Victor') {
+    console.log('Both conditions are true');
+} else {
+    console.log('One condition is false');
+}
