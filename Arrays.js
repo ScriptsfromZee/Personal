1d3b4f270522e1669b26f7be8889be7d4b935447
Zogam <derekchizogam@gmail.com>
2025-03-07 16:45:16 +0100

    Added Arrays

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+//This is one way to write arrays 
+const animals = ["cat", "dog", "bird", "sheep","goat","rat","lizard","pig"]
+
+// This is another way to write arrays
+const numbers = new Array(1, 2, 3, 4, 5,6,7,8,9);
+
+console.log(animals[0]) // This displays the first element in the array
+console.log(animals.length); // This tells us how many elements are in the array
+
+animals.push("bear");// This adds another element to the array
+console.log(animals)
