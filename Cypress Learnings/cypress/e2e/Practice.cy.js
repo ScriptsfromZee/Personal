153813f0d1e9c734c6596255edcec6f1b3cf9512
Zogam <derekchizogam@gmail.com>
2025-04-02 16:15:44 +0100

    Placed Order

--- Cypress Learnings/cypress/e2e/Practice.cy.js
+++ Cypress Learnings/cypress/e2e/Practice.cy.js
@@ -1,8 +1,14 @@
 describe("Practice Suite",() => {
-    it("New Things", () => {
+    it("Placed Order for Cashew", () => {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type("ca")
         cy.get(".product:visible").should('have.length', 4) // this checks that four visible items are shown when "ca" is serached for
-
-    })
+        cy.contains('.product-name', 'Cashews').parents('.product').find('button').click() // this looks for the product name that has "cashews" and adds it to cart
+        cy.get(".cart-icon").click()
+        cy.contains("PROCEED TO CHECKOUT").click()
+        cy.contains("Place Order").click()
+        cy.get("select").select("Philippines")
+        cy.get(".chkAgree").click()
+        cy.get("button").click()
+})     
 })
