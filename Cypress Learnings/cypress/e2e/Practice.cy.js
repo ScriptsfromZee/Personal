ac5888686a9120f51a40abf00fd2c3faa7236b1f
Zogam <derekchizogam@gmail.com>
2025-04-07 13:51:18 +0100

    More Comments

--- Cypress Learnings/cypress/e2e/Practice.cy.js
+++ Cypress Learnings/cypress/e2e/Practice.cy.js
@@ -1,5 +1,5 @@
 describe("Practice Suite 1",() => {
-    it("Placed Order for Cashew", () => {
+    it.only("Placed Order for Cashew", () => {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type("ca")
         cy.get(".product:visible").should('have.length', 4) // this checks that four visible items are shown when "ca" is serached for
@@ -7,14 +7,15 @@ describe("Practice Suite 1",() => {
         cy.get(".cart-icon").click() // This is the add to cart button 
         cy.contains("PROCEED TO CHECKOUT").click()
         cy.contains("Place Order").click()
-        cy.get("select").select("Philippines") // This selects philippines from the list of countries when the drop down is clicked. 
+        cy.get("select").select("Libya") // This selects philippines from the list of countries when the drop down is clicked. 
         cy.get(".chkAgree").check().should('be.checked')
         cy.get("button").click()
 })     
-    it.only("Practice Actions for Cypress", () => {
+    it("Practice Actions for Cypress", () => {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
         cy.get("#mousehover").invoke("show")
         cy.contains("Top").click({force: true}) // This force clicks the element named "Top" 
+        cy.url("https://rahulshettyacademy.com/AutomationPractice/#top") // This checks if the URL changes after the button is click
 
     })
 })
