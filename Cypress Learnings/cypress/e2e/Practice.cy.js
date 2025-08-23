84947f9688632406520b1e37e7f02fa3c9a891d0
Zogam <derekchizogam@gmail.com>
2025-08-23 12:22:55 +0100

    Added New Ways to Order Cashew

--- Cypress Learnings/cypress/e2e/Practice.cy.js
+++ Cypress Learnings/cypress/e2e/Practice.cy.js
@@ -6,7 +6,7 @@ describe("Practice Suite 1", () => {
     cy.get(".product:visible").should("have.length", 4); // this checks that four visible items are shown when "ca" is serached for
     cy.contains(".product-name", "Cashews")
       .parents(".product")
-      .find("button")
+      .find("button") // this adds cashews to the cart by searching for the product name that has "cashews" under the available products in the parent DOM
       .click(); // this looks for the product name that has "cashews" and adds it to cart
     cy.get(".cart-icon").click(); // This is the add to cart button
     cy.contains("PROCEED TO CHECKOUT").click();
@@ -19,7 +19,7 @@ describe("Practice Suite 1", () => {
     cy.wait(2000); // We have to wait because the site has to be load again after using method 1
      cy.get(".search-keyword").type("ca");
     cy.get(".product")
-  .filter(":contains('Cashews')")
+  .filter(":contains('Cashews')") // this scans the parent Dom and filters out the product that contains cashews and adds it to cart
   .find("button")
   .click()
   cy.get(".cart-icon").click(); // This is the add to cart button
@@ -32,7 +32,7 @@ describe("Practice Suite 1", () => {
 // The Third Way is by... 
  cy.wait(2000); // We have to wait because the site has to be load again after using method 2
 cy.get(".search-keyword").type("ca");
-cy.contains(".product", "Cashews")
+cy.contains(".product", "Cashews") // Here we use the "contains" method to search for cashews and add it to cart
   .find("button")
   .click();
     cy.get(".cart-icon").click(); // This is the add to cart button
@@ -62,4 +62,49 @@ cy.contains(".product", "Cashews")
     })
   })
 });
+
+// This is how you handle tables in cypress
+it('Getting Values from Tables',function() {
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.get('tr td:nth-child(2)').each(($e1, index, $list) => {
+
+    const text=$e1.text()
+    if(text.includes("Python"))
+    {
+
+        cy.get("tr td:nth-child(2)").eq(index).next().then(function(price)
+        {
+         const priceText=   price.text()
+         expect(priceText).to.equal('25')
+        })
+    }
+
+})
+
+})
+it('Verify date selection',()=>{
+
+        const monthNumber = "6";
+        const date = "15";
+        const year = "2027";
+        const expectedList = [monthNumber,date,year];
+
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        cy.wait(5000)
+        cy.get(".react-date-picker__inputGroup").click();
+
+        cy.get(".react-calendar__navigation__label").click();
+        cy.get(".react-calendar__navigation__label").click();
+        cy.contains("button",year).click();
+        cy.get(".react-calendar__year-view__months__month").eq(Number(monthNumber)-1).click();
+        cy.contains("abbr",date).click();
+
+        //Assertion
+        cy.get(".react-date-picker__inputGroup__input").each(($el,index)=>
+        {
+            cy.wrap($el).invoke('val').should('eq',expectedList[index]);
+        })
+})
+
+
 })
